06831cd3cd3727e72c6e1fbbe160a9b32a428293
Paulo Santos - AMD <afonso.mea@gmail.com>
2022-03-28 19:29:06 -0300

    Adiciona novo projeto MyFriends

---
+++ React Native/Projetos/aula5/MyFriends/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+
+      </View>
+      <Text></Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  header: {
+    height: 70,
+    backgroundColor: "green",
+  }
+  
+});
